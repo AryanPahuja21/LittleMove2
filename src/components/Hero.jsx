f071cb5d1b1418c1fdf667d3aae6b44e1f818727
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-21 18:25:22 +0530

    responsiveness added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-primary h-[88vh]'>
-      <main className='grid grid-cols-1 md:grid-cols-2 gap-5 p-24 items-center'>
+    <div className='bg-primary overflow-hidden'>
+      <main className='grid grid-cols-1 lg:grid-cols-2 md:my-8 gap-5 p-14 md:p-24 items-center'>
         <div>
-            <h1 className='text-6xl font-semibold text-white mr-10'>
+            <h1 className='text-4xl md:text-6xl font-semibold text-white md:mr-10'>
                 Revolutionize Your School Transportation with LittleMove!
             </h1>
-            <p className='text-3xl text-gray-600 mt-10 font-bold mr-10'>
+            <p className='text-xl md:text-3xl text-gray-600 mt-10 font-bold md:mr-10'>
                 Launching nationwide on April 1st! Sign up for your FREE trial and experience a smoother, greener school year! Limited spots available, register today!
             </p>
-            <div className='mt-16'>
+            <div className='mt-8 md:mt-16'>
                 <button className='bg-black text-white px-8 py-3 rounded-lg font-bold'>
                 Start your free trial now
                 </button>
             </div>
         </div>
         <div>
-            <img src="/heroimg.png" className='w-64 mx-auto mr-14 z-20' />
+            <img src="/heroimg.png" className='w-64 mx-auto lg:mr-14 z-20' />
         </div>
       </main>
     </div>
--- src/components/WhatIsLittleMove.jsx
+++ src/components/WhatIsLittleMove.jsx
@@ -43,17 +43,17 @@ const WhatIsLittleMove = () => {
     return (
         <div>
             <main>
-                <div className='grid grid-cols-1 md:grid-cols-2 m-24'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 m-24'>
                     <div>
-                        <h1 className='w-fit text-primary font-bold text-6xl'>What is LittleMove?</h1>
-                        <p className='mt-10 font-semibold text-2xl'>Little Move is a game-changing app-base platform that transforms school transportation into a stress-free experience for everyone involved. We harness cutting-edge technology to ensure:</p>
+                        <h1 className='w-fit text-primary font-bold text-4xl md:text-6xl'>What is LittleMove?</h1>
+                        <p className='mt-10 font-semibold text-xl md:text-2xl'>Little Move is a game-changing app-base platform that transforms school transportation into a stress-free experience for everyone involved. We harness cutting-edge technology to ensure:</p>
                     </div>
                     <img src='/bus.png' alt='' />
                 </div>
             </main>
-            <section className='grid grid-cols-3 gap-10 mx-24'>
+            <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-24'>
                 {items.map((item, index) => (
-                    <div key={index} className='shadow-md rounded-lg p-6 mb-4 bg-[#FFF7E8]'>
+                    <div key={index} className='shadow-md rounded-lg p-6 mb-4 bg-[#FFF7E8] scale-100 hover:scale-105 hover:bg-amber-100'>
                         {item.image && <div className='text-4xl mb-4'>{item.image}</div>}
                         <h1 className='text-xl text-primary font-bold mb-2'>{item.title}</h1>
                         <p className='text-gray-600 font-semibold'>{item.desc}</p>
